46a75a4a7ec650daa8f6157e28d4b786ef4d92c2
kumang subba <kumangsubba@gmail.com>
2026-01-21 10:26:24 +0200

    fix route-transition

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,25 +6,12 @@ import NavBar from "@/components/NavBar";
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 import Transition from "@/components/Transition";
-import { PropsWithChildren, useContext, useRef } from "react";
-import { LayoutRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import Footer from "@/components/Footer";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-montserrat",
 });
-
-function FrozenRouter(props: PropsWithChildren<{}>) {
-  const context = useContext(LayoutRouterContext);
-  const frozen = useRef(context).current;
-
-  return (
-    <LayoutRouterContext.Provider value={frozen}>
-      {props.children}
-    </LayoutRouterContext.Provider>
-  );
-}
 
 export default function RootLayout({
   children,
@@ -45,16 +32,14 @@ export default function RootLayout({
         <body
           className={cn(
             montserrat.className,
-            "bg-gradient-to-b from-indigo-900 via-black to-black font-montserrat text-white/95 bg-no-repeat"
+            "bg-gradient-to-b from-indigo-900 via-black to-black font-montserrat text-white/95 bg-no-repeat",
           )}
         >
           <NavBar />
           <AnimatePresence mode="wait">
             <motion.div key={path}>
-              <FrozenRouter>
-                <Transition />
-                {children}
-              </FrozenRouter>
+              <Transition />
+              {children}
             </motion.div>
           </AnimatePresence>
           <Footer />
